9073f581079dc7c4308984b29470831866ba2a79
illuminat3 <matthewrawson2@gmail.com>
2026-03-26 22:59:54 +0000

    import implementations

---
+++ clipforge_recorder/src/config/mod.rs
@@ -0,0 +1,9 @@
+mod loader;
+mod hotkeys;
+mod recording;
+mod storage;
+
+pub use loader::ConfigLoader;
+pub use hotkeys::HotkeysConfig;
+pub use recording::RecordingConfig;
+pub use storage::StorageConfig;
