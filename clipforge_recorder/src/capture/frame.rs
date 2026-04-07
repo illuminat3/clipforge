ea591f379e2297677863019ceea726e34c3c8ed6
Matthew <matthewrawson2@gmail.com>
2026-04-07 22:41:53 +0100

    Delete clipforge_recorder directory

--- clipforge_recorder/src/capture/frame.rs
+++
@@ -1,8 +0,0 @@
-use windows::Win32::Graphics::Direct3D11::ID3D11Texture2D;
-
-pub struct CapturedFrame {
-    pub texture: ID3D11Texture2D,
-    pub width: u32,
-    pub height: u32,
-    pub timestamp: i64,
-}
--- clipforge_recorder/src/config/mod.rs
+++
@@ -1,9 +0,0 @@
-mod loader;
-mod hotkeys;
-mod recording;
-mod storage;
-
-pub use loader::ConfigLoader;
-pub use hotkeys::HotkeysConfig;
-pub use recording::RecordingConfig;
-pub use storage::StorageConfig;
